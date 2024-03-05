a0b9321ae70d10d40d1f16b87ae15fe31ef8f580
ruijosecj <ruijosecj@gmail.com>
2024-03-05 08:57:03 -0300

    GET products with pagination

--- src/main/java/ruijosecj/controllers/ProductController.java
+++ src/main/java/ruijosecj/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package ruijosecj.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,11 @@ public class ProductController {
 		ProductDTO dto = service.findById(id);
 		return dto;
 	}
+	
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
+	}
+
 
 }
