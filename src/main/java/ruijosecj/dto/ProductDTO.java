ece137030920e05c9d3d6e15fe44ddfc24783ec1
ruijosecj <ruijosecj@gmail.com>
2024-05-20 09:52:21 -0300

    GET user logged

--- src/main/java/ruijosecj/dto/ProductDTO.java
+++ src/main/java/ruijosecj/dto/ProductDTO.java
@@ -1,7 +1,6 @@
 package ruijosecj.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import ruijosecj.entities.Product;
